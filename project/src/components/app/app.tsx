17e1c6d3ee775a829ea21909935ef89bcd523fe3
Nikita <nikita.krymov@bk.ru>
2022-02-13 19:56:00 +0300

    Отрисовал страницу

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,5 +1,16 @@
-function App(): JSX.Element {
-  return <p>Hello, world!</p>;
+import MainPage from '../main-page/main-page';
+import PageReview from '../add-review/add-review';
+
+type AppScreenProps = {
+  title: string,
+  genre: string,
+  year: number;
+};
+
+function App({genre,title,year}: AppScreenProps): JSX.Element {
+  return (
+    <><MainPage title={title} genre={genre} year={year} /><PageReview /></>
+  );
 }
 
 export default App;
--- project/src/index.tsx
+++ project/src/index.tsx
@@ -2,8 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app/app';
 
+const Setting = {
+  GENRE: 'Drama',
+  YEAR: 2014,
+  TITLE: 'The Grand Budapest Hotel',
+};
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App title={Setting.TITLE} genre={Setting.GENRE} year={Setting.YEAR} />
   </React.StrictMode>,
   document.getElementById('root'));
